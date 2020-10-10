09231bd59aa1d3d9bbaf1ed998b7ba0188524ff4
elaynelemos <elayne.l.lemos@gmail.com>
2020-10-10 10:34:05 -0300

    Changes to a nuxt project

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  pathPrefix: `/elaynelemos.github.io`,
-  siteMetadata: {
-    title: `Hi! This is Elayne. :D`,
-    description: `Personal website hosted at GitHub.`,
-    author: `@elaynelemos`,
-  },
-}
