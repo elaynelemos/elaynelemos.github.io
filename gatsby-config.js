aa968e018dfc16093c82792a5e4fbf488b5e975d
elaynelemos <elayne.l.lemos@gmail.com>
2020-10-09 21:00:18 -0300

    Adds initial node files

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pathPrefix: `/elaynelemos.github.io`,
+  siteMetadata: {
+    title: `Hi! This is Elayne. :D`,
+    description: `Personal website hosted at GitHub.`,
+    author: `@elaynelemos`,
+  },
+}
